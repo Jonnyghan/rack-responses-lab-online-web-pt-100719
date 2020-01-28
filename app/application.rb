1ced36009743144ad3ac929b79902adae20bd69f
Jonathan Ghandforoush <jonnyghan@gmail.com>
2020-01-28 22:16:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,5 @@
 require 'rack'
-require 'pry'
+
 # Something that responds to call, that's what Rack demands
 class Application
   def call(env)
@@ -7,7 +7,7 @@ class Application
   end
  
   def pretty_response
-    binding.pry
+    
     (Time.now.to_i < 12)?  ["<em>Good Morning!</em>"] : ["<em>Good Afternoon!</em>"]
   end
 end
